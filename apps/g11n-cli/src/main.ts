6d8d20af51743d5b929bf2deff68e62600dac163
Nicu Tofan <nicu.tofan@gmail.com>
2023-12-09 18:48:38 +0200

    Upgrade versions, first public reelase

--- apps/g11n-cli/src/main.ts
+++ apps/g11n-cli/src/main.ts
@@ -5,7 +5,7 @@ import { updateCommand } from "./update";
 export const program = new Command();
 program
     .name('g11n-cli')
-    .version('0.0.12')
+    .version('0.1.0')
     .description('Manage globalization resources')
 
 program.addCommand(updateCommand());
