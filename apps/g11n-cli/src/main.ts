d63b563ef1d5c67d9756f41e61e6cd99c3fab1ab
Nicu Tofan <nicu.tofan@gmail.com>
2023-12-14 08:47:11 +0200

    failed to update package.json

--- apps/g11n-cli/src/main.ts
+++ apps/g11n-cli/src/main.ts
@@ -5,7 +5,7 @@ import { updateCommand } from "./update";
 export const program = new Command();
 program
     .name('g11n-cli')
-    .version('0.1.1')
+    .version('0.1.2')
     .description('Manage globalization resources')
 
 program.addCommand(updateCommand());
