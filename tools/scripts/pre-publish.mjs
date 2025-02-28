1c430614677e2011b944bff7d2bfc7b360c37f9e
Nicu Tofan <nicu.tofan@gmail.com>
2025-02-28 11:23:45 +0200

    Bump version to undefined

--- tools/scripts/pre-publish.mjs
+++ tools/scripts/pre-publish.mjs
@@ -1,9 +1,16 @@
 /**
- * This is a node script that updates the version of all the projects in the workspace.
- * It also creates a commit and a tag for the new version.
+ * This is a node script that updates the version of all the projects in the
+ * workspace. It also creates a commit and a tag for the new version.
  *
- * Executing this script: node path/to/pre-publish.mjs {version}
- * or: pnpm pp {version}
+ * To set all versions to a particular number:
+ *
+ *      node path/to/pre-publish.mjs {version}
+ *      pnpm pp {version}
+ *
+ * To increment the patch version:
+ *
+ *      node path/to/pre-publish.mjs
+ *      pnpm pp
  */
 
 import { execSync } from 'child_process';
@@ -12,29 +19,38 @@ import chalk from 'chalk';
 import devkit from '@nx/devkit';
 const { readCachedProjectGraph } = devkit;
 
+const validVersion = /^(\d+)\.(\d+)\.(\d+)$/;
+
 
 const args = process.argv.slice(2);
-if (!args.length) {
-    console.error(chalk.red('Please provide a version number as a command-line argument.'));
-    process.exit(1);
+let version;
+if (args.length) {
+    // Make sure that the version is valid
+    if (!validVersion.test(version)) {
+        console.error(chalk.red(`Invalid version number: ${version}`));
+        process.exit(1);
+    }
+    version = args[0];
 }
-const version = args[0];
 
-// Make sure that the version is valid
-const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
-if (!validVersion.test(version)) {
-    console.error(chalk.red(`Invalid version number: ${version}`));
-    process.exit(1);
-}
 
 const graph = readCachedProjectGraph();
 for (const node of Object.values(graph.nodes)) {
     const prjPath = node.data?.root;
     if (prjPath) {
         const json = JSON.parse(readFileSync(prjPath + `/package.json`).toString());
-        json.version = version;
-        writeFileSync(prjPath + `package.json`, JSON.stringify(json, null, 2));
 
+        if (version) {
+            json.version = version;
+        } else {
+            const prevVersion = json.version;
+            const match = validVersion.exec(prevVersion);
+            const major = parseInt(match[1]);
+            const minor = parseInt(match[2]);
+            const patch = parseInt(match[3]);
+            json.version = `${major}.${minor}.${patch + 1}`;
+        }
+        writeFileSync(prjPath + `package.json`, JSON.stringify(json, null, 2));
         console.log(chalk.green(`Updated version in ${node.name} to ${version}`));
     }
 }
