60bac317565bdfa534ecbd5425c19c67dbde21b7
Nicu Tofan <nicu.tofan@gmail.com>
2025-02-28 11:05:59 +0200

    Bump version to 0.1.7

---
+++ tools/scripts/pre-publish.mjs
@@ -0,0 +1,37 @@
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+import chalk from 'chalk';
+import devkit from '@nx/devkit';
+const { readCachedProjectGraph } = devkit;
+
+
+const args = process.argv.slice(2);
+if (!args.length) {
+    console.error(chalk.red('Please provide a version number as a command-line argument.'));
+    process.exit(1);
+}
+const version = args[0];
+
+// Make sure that the version is valid
+const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+if (!validVersion.test(version)) {
+    console.error(chalk.red(`Invalid version number: ${version}`));
+    process.exit(1);
+}
+
+const graph = readCachedProjectGraph();
+for (const node of Object.values(graph.nodes)) {
+    const prjPath = node.data?.root;
+    if (prjPath) {
+        const json = JSON.parse(readFileSync(prjPath + `/package.json`).toString());
+        json.version = version;
+        writeFileSync(prjPath + `package.json`, JSON.stringify(json, null, 2));
+
+        console.log(chalk.green(`Updated version in ${node.name} to ${version}`));
+    }
+}
+
+execSync(`git add .`);
+execSync(`git commit -m "Bump version to ${version}"`);
+execSync(`git tag -a v${version} -m "Version ${version}"`);
+execSync(`git push --tag`);
